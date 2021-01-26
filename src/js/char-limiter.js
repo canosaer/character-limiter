4b1923097fd5fe1c827a1df12a54b1a12616dcdc
canosaer <canosaer@gmail.com>
2021-01-25 23:52:16 -0500

    working version

--- src/js/char-limiter.js
+++ src/js/char-limiter.js
@@ -8,11 +8,11 @@
  */
 
 class CharLimiter{
-
+ 
     element = null
     options = {}
     DEFAULTS = {
-        limit: 10,
+        limit: 100,
         messageColor: `red`,
         warningAt: 10
     }
@@ -32,11 +32,16 @@ class CharLimiter{
             ...options
         }
         this.options.limit = parseInt(this.options.limit)
-
+        if(!options.warningAt){
+            this.options.warningAt = Math.ceil(this.options.limit*0.1)
+            if (this.options.warningAt < 5) this.options.warningAt = 5
+        }
+        
         this.setup()
     }
 
     setup() {
+        
         // add a new message field to the DOM
         // const message = document.createElement(`span`)
         // message.classList.add(`message`)
@@ -46,21 +51,50 @@ class CharLimiter{
         // setup an event listener on the input element
 
         const charUsageSpans = document.querySelectorAll(`.char-usage__count`)
+        const charUsagePs = document.querySelectorAll(`.char-usage`)
         const allINPUTS = document.querySelectorAll(`[data-limit]`)
-
-        let j=0
+        
 
         for(let i=0;i<allINPUTS.length;i++){
             charUsageSpans[i].textContent = allINPUTS[i].dataset.limit
         }
 
         this.handleKeyUp = (evt) => {
+            
+            let defaultMessageColor = ``
+            let defaultInputColor = ``
+            let j=0
             const curValue = this.element.value
             const curCount = curValue.length
             for(let i=0;i<allINPUTS.length;i++){
-                if (allINPUTS[i] === this.element) {j=i}
+                if (allINPUTS[i] === this.element) j=i
             }
-            charUsageSpans[j].textContent = this.options.limit - curCount;
+            let charsLeft = this.options.limit - curCount
+            charUsageSpans[j].textContent = charsLeft
+            
+            if (charsLeft <= this.options.warningAt) {
+                if (charUsagePs[j].style.color != this.options.messageColor){
+                    defaultMessageColor = charUsagePs[j].style.color
+                    charUsagePs[j].style.color = this.options.messageColor
+                }
+                if(charsLeft <= 0){
+                    if (allINPUTS[j].style.backgroundColor != this.options.messageColor){
+                        defaultInputColor = allINPUTS[j].style.backgroundColor
+                        allINPUTS[j].style.backgroundColor = this.options.messageColor
+                    }
+                    allINPUTS[j].value = this.element.value.substring(0,this.options.limit)
+                }
+                else{
+                    if (allINPUTS[j].style.backgroundColor != defaultInputColor){
+                        allINPUTS[j].style.backgroundColor = defaultInputColor
+                    }
+                }
+            }
+            else if(charsLeft > this.options.warningAt){ 
+                if (charUsagePs[j].style.color != defaultMessageColor){
+                    charUsagePs[j].style.color = defaultMessageColor
+                }
+            }
         }
 
         this.element.addEventListener(`keyup`, this.handleKeyUp)
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,4 @@ const allInputFields = document.querySelectorAll(`[data-limit]`)
 allInputFields.forEach((element) => {
     const charLimited = new CharLimiter(element, element.dataset)
 })
+
